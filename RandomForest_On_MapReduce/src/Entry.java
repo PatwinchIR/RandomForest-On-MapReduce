7dc438613473e82412dc9e8843514a4833440a1b
Dee Dong <ddong1@andrew.cmu.edu>
2017-03-13 15:23:43 -0400

    Added DecisionTree basic structure.

---
+++ RandomForest_On_MapReduce/src/Entry.java
@@ -0,0 +1,30 @@
+/**
+ * Created by d_d on 3/1/17.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class is specifically for row in dataset, attributes is a list of Double value, because of
+ * continuous/numerical attributes.
+ * label is a String indicating the label.
+ */
+class Entry {
+    List<CellData> attributes;
+    String label;
+
+    Entry() {
+        this.attributes = new ArrayList<>();
+        this.label = null;
+    }
+
+    public String toString(String delimiter) {
+        String returnedString = "";
+        for (CellData cd: attributes) {
+            returnedString += (cd.toString() + delimiter);
+        }
+        returnedString += label;
+        return returnedString;
+    }
+}
